def2098bfc4110e38e44edd5d231e8183d61225c
fde7486 <family711ok@gamil.com>
2020-09-29 13:16:58 +0800

    新增上課文件

--- 課程學習/影像處理/code/0929_02.py
+++ 課程學習/影像處理/code/0929_02.py
@@ -5,7 +5,7 @@ import math
 img1 = cv2.imread("building.jpg",-1)
 img2 = img1.copy()
 edges = cv2.Canny(img1,50,200)
-lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 80, minLineLength=30, maxLineGap=10)
+lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 90, minLineLength=50, maxLineGap=10)
 
 for line in lines:
     for x1,y1,x2,y2 in line:
